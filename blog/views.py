9261da0abbc4421180f263c776774f41b7dcf412
8471919 <7471919@naver.com>
2022-03-18 16:42:10 +0900

    블로그 상세 페이지 작성#0

--- blog/views.py
+++ blog/views.py
@@ -14,3 +14,14 @@ def index(request) :
         }
     )
 
+def single_post_page(request, pk):
+    #Post의 pk의 값이 매개변수로받은 pk와 같은 것을 뽑아온다.
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post
+        }
+    )
