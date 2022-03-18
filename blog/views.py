5376ce00fa9bdbc5b708011bd0d60d2944e5f1c4
8471919 <7471919@naver.com>
2022-03-18 17:11:56 +0900

    프로그램 CBV로 리팩토링#0

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 def index(request) :
     posts = Post.objects.all().order_by('-pk')
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post_list.html',
         {
             'posts': posts,
         }
@@ -20,7 +29,7 @@ def single_post_page(request, pk):
 
     return render(
         request,
-        'blog/single_post_page.html',
+        'blog/post_detail.html',
         {
             'post': post
         }
