ddd811f235520f046b8bbede1ffccd68c20ea666
Michael <7471919@naver.com>
2022-04-08 15:42:37 +0900

    Category 클래스 생성

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Category(models.Model) :
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
