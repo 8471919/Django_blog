302cd22ff7476a8267d72989f887a554340cabb6
Michael <7471919@naver.com>
2022-04-15 17:36:04 +0900

    PostCreat, PostUpdate를 생성하고 그에 맞게 template 업데이트

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
 
     #path('', views.index),
     path('category/<str:slug>', views.categories_page),
-    path('tag/<str:slug>/', views.tag_page)
+    path('tag/<str:slug>/', views.tag_page),
+    path('create_post/', views.PostCreate.as_view()),
+    path('update_post/<int:pk>', views.PostUpdate.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect
 from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 
 # Create your views here.
+#createview를 사용하면 기본 template가 post_form.html 이다.
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin,CreateView):
+    model = Post
+    fields = ['title', 'hook_msg', 'content', 'head_image', 'file_upload', 'category']
+
+    #현재사용자의 등급이 staff거나 superuser인지 리턴
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_msg', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    template_name = "blog/post_form_update.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
 
 class PostList(ListView):
     model = Post
